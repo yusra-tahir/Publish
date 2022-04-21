c6bd80593ed6b448052da8f2e1e4a6cd067f3276
Yusra <yusra-tahir@hotmail.co.uk>
2022-04-21 10:59:46 +0100

    updated endpoints

--- api/dbConfig/dev_seeds.sql
+++ api/dbConfig/dev_seeds.sql
@@ -3,22 +3,22 @@ VALUES
 (
     'The Hitchhikers Guide to the Galaxy', 
     'Douglas Adams', 
-    $str$Earthman Arthur Dent is saved by his friend, Ford Prefect — an alien researcher for the titular Hitchhiker's Guide to the Galaxy, which provides info on every planet in the galaxy—from the Earth just before it is destroyed by the alien Vogons.$str$
+    $str$Earthman Arthur Dent is saved by his friend, Ford Prefect — an alien researcher for the titular Hitchhiker's Guide to the Galaxy.$str$
 ),
 (
     'The Restaurant at the End of the Universe', 
     'Douglas Adams', 
-    $str$Arthur Dent, Ford Prefect, Trillian, and Zaphod Beeblebrox leave the planet Magrathea on the Heart of Gold. A Vogon ship attacks them, and Arthur's attempt to have the ship computer make him a cup of tea leaves the Heart of Gold unable to fight it off. Zaphod calls up his ancestor Zaphod Beeblebrox the Fourth to rescue them. Zaphod and Marvin vanish, leaving the others on the ship in a black void.$str$
+    $str$Arthur Dent, Ford Prefect, Trillian, and Zaphod Beeblebrox leave the planet Magrathea on the Heart of Gold.$str$
 ),
 (
     'War and Peace',
     'Leo Tolstoy',
-    $str$Pierre Bezukhov is the illegitimate son of a wealthy count, who is dying after a series of strokes. Pierre is about to become embroiled in a struggle for his inheritance. Educated abroad at his father's expense following his mother's death, Pierre is kindhearted but socially awkward, and finds it difficult to integrate into Petersburg society.$str$
+    $str$Pierre Bezukhov is the illegitimate son of a wealthy count, who is dying after a series of strokes. Pierre is about to become embroiled in a struggle for his inheritance.$str$
 ),
 (
     'A Shadow of Gulls',
     'Patricia Finney',
-    $str$The story of Lugh Mac Romain, harper and reluctant warrior, as he struggles to escape the curse of the Queen Maeve of Connaught for killing her king and is caught up in the famous Cattle Raid of Cooley by his friendship with the great Champion of Ulster, Cuchulain of Muirthemne.$str$
+    $str$The story of Lugh Mac Romain, harper and reluctant warrior, as he struggles to escape the curse of the Queen Maeve of Connaught for killing her king.$str$
 ),
 (
     'I, Jack',
--- db/create_posts.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS posts;
-
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    title text NOT NULL,
-    writer varchar(255) NOT NULL,
-    content varchar(3000) NOT NULL
-);
